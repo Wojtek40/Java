9da45865d81148f6b33c20330134e016c81be1a6
Wb <m@m.me>
2023-09-05 10:51:51 +0200

    Created GenericHolder

--- src/com/company/GenericHolder.java
+++ src/com/company/GenericHolder.java
@@ -1,2 +1,17 @@
-package com.company;public class GenericHolder {
+package com.company;
+
+public class GenericHolder<T> {
+    private T obj;
+
+    public GenericHolder(T obj) {
+        this.obj = obj;
+    }
+
+    public T getObj() {
+        return obj;
+    }
+
+    public void setObj(T obj) {
+        this.obj = obj;
+    }
 }
