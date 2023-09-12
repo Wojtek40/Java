222d8ab6290038f06f09e164a418a9c0b7a26ba0
Uczen <uczen>
2023-09-12 11:07:09 +0200

    Zadanie 2 (zad. 1, ale generycznie)

--- src/com/company/Compare.java
+++ src/com/company/Compare.java
@@ -1,51 +1,58 @@
 package com.company;
 
 public class Compare {
-    public Compare() {
-    }
-    public void compareValues(int a, int b) {
-        if (a > b) {
-            System.out.println("a = " + a + " jest większe od b = " + b);
-        } else if (a < b) {
-            System.out.println("a = " + a + " jest mniejsze od b = " + b);
-        } else {
-            System.out.println("a = " + a + " jest równe b = " + b);
-        }
-    }
-    public void compareValues(float a, float b) {
-        if (a > b) {
-            System.out.println("a = " + a + " jest większe od b = " + b);
-        } else if (a < b) {
-            System.out.println("a = " + a + " jest mniejsze od b = " + b);
-        } else {
-            System.out.println("a = " + a + " jest równe b = " + b);
-        }
-    }
-    public void compareValues(double a, double b) {
-        if (a > b) {
-            System.out.println("a = " + a + " jest większe od b = " + b);
-        } else if (a < b) {
-            System.out.println("a = " + a + " jest mniejsze od b = " + b);
-        } else {
-            System.out.println("a = " + a + " jest równe b = " + b);
-        }
-    }
-    public void compareValues(byte a, byte b) {
-        if (a > b) {
-            System.out.println("a = " + a + " jest większe od b = " + b);
-        } else if (a < b) {
-            System.out.println("a = " + a + " jest mniejsze od b = " + b);
-        } else {
-            System.out.println("a = " + a + " jest równe b = " + b);
-        }
-    }
-    public void compareValues(String a, String b) {
+//    public void compareValues(Integer a, Integer b) {
+//        if (a > b) {
+//            System.out.printf("a = %s jest większe od b = %s\n", a,  b);
+//        } else if (a < b) {
+//            System.out.printf("a = %s jest mniejsze od b = %s\n", a,  b);
+//        } else {
+//            System.out.printf("a = %s jest równe b = %s\n", a,  b);
+//        }
+//    }
+//    public void compareValues(float a, float b) {
+//        if (a > b) {
+//            System.out.printf("a = %s jest większe od b = %s\n", a,  b);
+//        } else if (a < b) {
+//            System.out.printf("a = %s jest mniejsze od b = %s\n", a,  b);
+//        } else {
+//            System.out.printf("a = %s jest równe b = %s\n", a,  b);
+//        }
+//    }
+//    public void compareValues(double a, double b) {
+//        if (a > b) {
+//            System.out.printf("a = %s jest większe od b = %s\n", a,  b);
+//        } else if (a < b) {
+//            System.out.printf("a = %s jest mniejsze od b = %s\n", a,  b);
+//        } else {
+//            System.out.printf("a = %s jest równe b = %s\n", a,  b);
+//        }
+//    }
+//    public void compareValues(byte a, byte b) {
+//        if (a > b) {
+//            System.out.printf("a = %s jest większe od b = %s\n", a,  b);
+//        } else if (a < b) {
+//            System.out.printf("a = %s jest mniejsze od b = %s\n", a,  b);
+//        } else {
+//            System.out.printf("a = %s jest równe b = %s\n", a,  b);
+//        }
+//    }
+//    public void compareValues(String a, String b) {
+//        if (a.compareTo(b) > 0) {
+//            System.out.printf("a = %s jest większe od b = %s\n", a,  b);
+//        } else if (a.compareTo(b) < 0) {
+//            System.out.printf("a = %s jest mniejsze od b = %s\n", a,  b);
+//        } else {
+//            System.out.printf("a = %s jest równe b = %s\n", a,  b);
+//        }
+//    }
+    public <AnyType extends Comparable> void compareValues(AnyType a, AnyType b) {
         if (a.compareTo(b) > 0) {
-            System.out.println("a = " + a + " jest większe od b = " + b);
+            System.out.printf("a = %s jest większe od b = %s\n", a,  b);
         } else if (a.compareTo(b) < 0) {
-            System.out.println("a = " + a + " jest mniejsze od b = " + b);
+            System.out.printf("a = %s jest mniejsze od b = %s\n", a,  b);
         } else {
-            System.out.println("a = " + a + " jest równe b = " + b);
+            System.out.printf("a = %s jest równe b = %s\n", a,  b);
         }
     }
 }
