05aab2386216be45754918ab5fbd48bf9bda02fc
Uczen <wzbury@wp.pl>
2023-09-19 10:45:40 +0200

    Zadanie 1.3 już działa

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -1,50 +1,64 @@
 package com.company;
 
-public class Car implements Comparable{
+import java.time.LocalTime;
+
+public class  Car<
+        WeightType extends Number & Comparable,
+        WidthType extends Number & Comparable,
+        LengthType extends Number & Comparable> implements Comparable<Car>{
+
     @Override
-    public int compareTo(Object o) {
-        return 0;
+    public int compareTo(Car car) {
+        LocalTime myObj = LocalTime.of(15, 0);//LocalTime.now();
+
+        if(myObj.isBefore(LocalTime.NOON)){
+            return masa.compareTo(car.masa);
+        } else {
+            if (masa.compareTo(car.masa) == 0) {
+                if (dlugosc.compareTo(car.dlugosc) == 0) {
+                    return szerokosc.compareTo(car.szerokosc);
+                }
+                return dlugosc.compareTo(car.dlugosc);
+            }
+            return masa.compareTo(car.masa);
+        }
     }
-    private int id, masa, dlugosc, szerokosc;
+
+
+    private WeightType masa;
+    private WidthType szerokosc;
+    private LengthType dlugosc;
     private String marka;
 
-    public Car(int id, int masa, int dlugosc, int szerokosc, String marka) {
-        this.id = id;
+    public Car(String marka, WeightType masa, LengthType dlugosc, WidthType szerokosc) {
         this.masa = masa;
         this.dlugosc = dlugosc;
         this.szerokosc = szerokosc;
         this.marka = marka;
     }
 
-    public int getId() {
-        return id;
-    }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getMasa() {
+    public Number getMasa() {
         return masa;
     }
 
-    public void setMasa(int masa) {
+    public void setMasa(WeightType masa) {
         this.masa = masa;
     }
 
-    public int getDlugosc() {
+    public LengthType getDlugosc() {
         return dlugosc;
     }
 
-    public void setDlugosc(int dlugosc) {
+    public void setDlugosc(LengthType dlugosc) {
         this.dlugosc = dlugosc;
     }
 
-    public int getSzerokosc() {
+    public WidthType getSzerokosc() {
         return szerokosc;
     }
 
-    public void setSzerokosc(int szerokosc) {
+    public void setSzerokosc(WidthType szerokosc) {
         this.szerokosc = szerokosc;
     }
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,40 +2,41 @@ package com.company;
 
 public class Main {
 
+
     public static void main(String[] args) {
 //        ćwiczenia
 
-//        GenericHolder<Integer> genericHolder = new GenericHolder(213);
-//        System.out.println(genericHolder.getObj() + 1);
-//        System.out.println(genericHolder.getObj().getClass().getSimpleName());
-//        genericHolder.setObj(333);
-//        System.out.println(genericHolder.getObj());
-//        System.out.println(genericHolder.getObj().getClass().getSimpleName());
-//
-//        ObjectHolder objectHolder = new ObjectHolder(2000);
-//        System.out.println((int)objectHolder.getObj() + 1);
-//        System.out.println(objectHolder.getObj().getClass().getSimpleName());
-//
-//        objectHolder.setObj(333);
-//        System.out.println((int) objectHolder.getObj() + 1);
-//        System.out.println(objectHolder.getObj().getClass().getSimpleName());
+/*        GenericHolder<Integer> genericHolder = new GenericHolder(213);
+        System.out.println(genericHolder.getObj() + 1);
+        System.out.println(genericHolder.getObj().getClass().getSimpleName());
+        genericHolder.setObj(333);
+        System.out.println(genericHolder.getObj());
+        System.out.println(genericHolder.getObj().getClass().getSimpleName());
+
+        ObjectHolder objectHolder = new ObjectHolder(2000);
+        System.out.println((int)objectHolder.getObj() + 1);
+        System.out.println(objectHolder.getObj().getClass().getSimpleName());
+
+        objectHolder.setObj(333);
+        System.out.println((int) objectHolder.getObj() + 1);
+        System.out.println(objectHolder.getObj().getClass().getSimpleName());*/
 
         /** Zadanie 1.1 - 1.2 */
 
-        Compare MyComparator = new Compare();
+/*        Compare MyComparator = new Compare();
         MyComparator.compareValues(1,1);
         MyComparator.compareValues(1.1d,2.2d);
         MyComparator.compareValues(1.0f,0.1f);
         MyComparator.compareValues((byte)1, (byte)2);
-        MyComparator.compareValues("00001", "2");
+        MyComparator.compareValues("00001", "2");*/
 
-        /** Zadanie 1.3 */
-        /*
+/*
+        * Zadanie 1.3
+*/
         Compare MyComparator = new Compare();
         MyComparator.compareValues(new Car("BMW", 100.1d, 100, 100), new Car("AUDI", 100d, 100, 100));
         MyComparator.compareValues(new Car("BMW", 100, 101, 100), new Car("AUDI", 100, 100, 100));
         MyComparator.compareValues(new Car("BMW", 100, 100, 999), new Car("AUDI", 100, 101, 100));
         MyComparator.compareValues(new Car("BMW", 100.1f, 100, (byte)101), new Car("AUDI", 100.1f, 100, (byte)100));
-         */
     }
 }
